a1b4a03c419798e7e50eec92f145ced2bde0fcb6
HUAJIAN <1415885403@qq.com>
2019-10-29 19:01:50 +0800

    add projection temp

--- tinyrenderer-barycentricTriangle/main.cpp
+++ tinyrenderer-barycentricTriangle/main.cpp
@@ -61,17 +61,30 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     }
 }
 
-Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+// Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+//     Vec3f s[2];
+//     for (int i=2; i--; ) {
+//         s[i][0] = C[i]-A[i];
+//         s[i][1] = B[i]-A[i];
+//         s[i][2] = A[i]-P[i];
+//     }
+//     Vec3f u = cross(s[0], s[1]);
+//     if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+//         return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+//     return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+// }
+
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P){
     Vec3f s[2];
-    for (int i=2; i--; ) {
-        s[i][0] = C[i]-A[i];
-        s[i][1] = B[i]-A[i];
-        s[i][2] = A[i]-P[i];
+    for (int i=2; i--; )
+    {
+        /* code */
+        s[i][0] = A[i] - B[i];
+        s[i][1] = B[i] - C[i];
+        s[i][2] = C[i] - P[i];
     }
-    Vec3f u = cross(s[0], s[1]);
-    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
-    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+    Vec3f u = cross(s[0],s[1]);
+    return Vec3f(1 - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
 void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
@@ -90,7 +103,7 @@ void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
             Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
             if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
             P.z = 0;
-            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];  //todo:获得zbuffer -- 三角形坐标z值分别乘重心计算值？
             if (zbuffer[int(P.x+P.y*width)]<P.z) {
                 zbuffer[int(P.x+P.y*width)] = P.z;
                 image.set(P.x, P.y, color);
